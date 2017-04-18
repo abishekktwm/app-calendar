6d799423916ac3101610a703e3fdd1730aa4ad90
Raphael Costa <raphael@raphaelcosta.net>
2017-04-18 18:24:52 -0300

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,7 @@ module.exports = {
       {
         test: /.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
+        exclude: /node_modules/
       },
     ]
   },
