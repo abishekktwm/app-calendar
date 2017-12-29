cff7e079f961db6be5d6d443bb6fe1cb6e20438a
Kleber Kihara <kleber.kihara@pipefy.com>
2017-12-29 11:29:55 -0200

    Setup Babel/Flow correctly and update some tests

--- __mocks__/big_calendar.js
+++ __mocks__/big_calendar.js
@@ -3,3 +3,5 @@ export const onNavigate = jest.fn();
 export const onViewChange = jest.fn();
 export const view = 'month';
 export const views = ['month', 'week', 'day', 'agenda'];
+
+export default { label, onNavigate, onViewChange, view, views };
